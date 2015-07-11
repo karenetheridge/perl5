904c42c1fa7e1907bddd725d40fd98183561f56c
David Mitchell <davem@iabyn.com>
2015-07-11 15:30:38 +0100

    pp_entersub(): eliminate a label
    replace:
    
      retry:
        if (A) die;
        if (B) {
            ...;
            goto retry;
        }
    
    with
    
        while (B) {
            ...;
        }
    
        if (A) die;
    
    it's functionally equivalent except that the A test is now only
    tried after we have been successful at B. This is ok, because
    A is testing for an uncloned closure prototype, while B is looking
    for a CV stub which needs autoloading,and it doesn't rally matter
    which we test for first.

--- pp_hot.c
+++ pp_hot.c
@@ -3108,10 +3108,7 @@ PP(pp_entersub)
 
     ENTER;
 
-  retry:
-    if (UNLIKELY(CvCLONE(cv) && ! CvCLONED(cv)))
-	DIE(aTHX_ "Closure prototype called");
-    if (UNLIKELY(!CvROOT(cv) && !CvXSUB(cv))) {
+    while (UNLIKELY(!CvROOT(cv) && !CvXSUB(cv))) {
 	GV* autogv;
 	SV* sub_name;
 
@@ -3144,8 +3141,10 @@ PP(pp_entersub)
 	}
 	if (!cv)
 	    goto sorry;
-	goto retry;
     }
+
+    if (UNLIKELY(CvCLONE(cv) && ! CvCLONED(cv)))
+	DIE(aTHX_ "Closure prototype called");
 
     if (UNLIKELY((PL_op->op_private & OPpENTERSUB_DB) && GvCV(PL_DBsub)
             && !CvNODEBUG(cv)))
